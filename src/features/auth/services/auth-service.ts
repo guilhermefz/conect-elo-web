aee09793e098a60dd61677bd6b6b3ea7b836aee8
Affonso Ely <affonsoely@gmail.com>
2026-06-22 21:22:21 -0300

    feat: adicionar metodo registro e tipar credenciais de login no auth-service

--- src/features/auth/services/auth-service.ts
+++ src/features/auth/services/auth-service.ts
@@ -1,8 +1,23 @@
-import  api  from "../../../lib/axios";
+import api from "../../../lib/axios";
+
+interface LoginDto {
+  email: string;
+  password: string;
+}
+
+interface RegistroDto {
+  email: string;
+  password: string;
+}
 
 export const authService = {
-    login: async (credentials: any) => {
-        const response = await api.post("/api/Autenticacao/Login", credentials);
-        return response;
-    }
+  login: async (credentials: LoginDto) => {
+    const response = await api.post("/api/Autenticacao/Login", credentials);
+    return response;
+  },
+
+  registro: async (dados: RegistroDto) => {
+    const response = await api.post("/register", dados);
+    return response;
+  },
 }
